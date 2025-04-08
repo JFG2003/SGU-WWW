1389b4e0b6c12747eae2a4254913cddd722dad70
JFG2003 <juanfelipeguanchaduran@gmail.com>
2025-04-07 20:40:52 -0500

    adicion deslpiegue local-render

--- App/main.py
+++ App/main.py
@@ -1,21 +1,34 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware  # 👈 Importar CORS
 from App.router import books
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Montar la carpeta donde está tu index.html
+# 👇 Configurar CORS
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://sgu-www.onrender.com",  # 🔥 tu dominio en línea
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # solo permitir estos orígenes
+    allow_credentials=True,
+    allow_methods=["*"],         # permitir todos los métodos (GET, POST, PUT, DELETE)
+    allow_headers=["*"],         # permitir todas las cabeceras
+)
+
+# Montar carpeta de archivos estáticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Incluir las rutas de libros
 app.include_router(books.router)
 
-
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
+# Montar el index.html en la raíz "/"
 @app.get("/", response_class=HTMLResponse)
 def root():
     with open("static/index.html", "r", encoding="utf-8") as f:
